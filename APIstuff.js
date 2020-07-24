52bfbf947555afab0d541a16a85fe4eb4315c87b
Nev <67838965+NevMoney@users.noreply.github.com>
2020-07-24 17:43:35 -0400

    Improved display
    Improved display of the page with correct number and currency displays

--- APIstuff.js
+++ APIstuff.js
@@ -12,25 +12,27 @@ function displayAPI() {
 
     for(let i = 0; i < data.length; i++){
       let color24 = "";
-      if (data[i].price_change_percentage_24h > 0){
-        color24 = "positive";
-      }
-      else if (data[i].price_change_percentage_24h < 0) {
-        color24 = "negative";
-      }
+        if (data[i].price_change_percentage_24h > 0){
+          color24 = "positive";
+        }
+        else if (data[i].price_change_percentage_24h < 0) {
+          color24 = "negative";
+        }
 
-      x = new Intl.NumberFormat().format(data[i].market_cap);
-
+      mc = new Intl.NumberFormat().format(data[i].market_cap);
+      p = new Intl.NumberFormat().format(data[i].current_price);
+      v = new Intl.NumberFormat().format(data[i].total_volume);
+      cs = new Intl.NumberFormat().format(data[i].circulating_supply);
 
       $("#myDisplay").append(
         `<tr>
-        <th scope="row">${i}</th>
-        <td><img width=18px src=${data[i].image}>&nbsp;<strong>${data[i].name}</stong></td>
-        <td>$${data[i].market_cap}</td>
-        <td>$${data[i].current_price}</td>
-        <td>$${data[i].total_volume}</td>
-        <td>${data[i].circulating_supply} ${data[i].symbol.toUpperCase()}</td>
-        <td class="${color24}">${data[i].price_change_percentage_24h.toFixed(2)}%</td>
+          <th scope="row" class="notbold">${i+1}</th>
+          <td><img width=16px src=${data[i].image}>&nbsp;<strong>${data[i].name}</stong></td>
+          <td>$${mc}</td>
+          <td>$${p}</td>
+          <td>$${v}</td>
+          <td>${cs} ${data[i].symbol.toUpperCase()}</td>
+          <td class="${color24}">${data[i].price_change_percentage_24h.toFixed(2)}%</td>
         </tr>`
         );
       }
