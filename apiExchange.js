6dabef276d0e7604c20246f0324096352780e221
Nev <67838965+NevMoney@users.noreply.github.com>
2020-07-25 11:41:21 -0400

    exchanges listed
    Exchange page created and exchanges listed on the page

--- apiExchange.js
+++ apiExchange.js
@@ -1,6 +1,3 @@
-//here we'll be pulling exchange info and displaying it on the website
-
-
 let BASE_URL = "https://api.coingecko.com/api/v3";
 let EXCHNAGE_ENDPOINT = "/exchanges";
 let urlEx = BASE_URL + EXCHNAGE_ENDPOINT;
@@ -8,61 +5,25 @@ let urlEx = BASE_URL + EXCHNAGE_ENDPOINT;
 
 function displayExchange() {
 
-  fetch(url)
+  fetch(urlEx)
     .then(function (res) {
       res.json().then(function (data) {
+      for(let i = 0; i < data.length; i++)
 
       $("#myExDisplay").append(
         `<tr>
-        <th scope="row">${i}</th>
+        <th scope="row" class="notbold">${i+1}</th>
         <td><img width=18px src=${data[i].image}>&nbsp;<strong>${data[i].name}</stong></td>
-        <td>$${data[i].trust_score}</td>
-        <td>$${data[i].trade_volume_24h_btc}</td>
-        <td>$${data[i].year_established}</td>
+        <td>${data[i].trust_score}</td>
+        <td>${data[i].trade_volume_24h_btc.toFixed(2)} BTC</td>
+        <td>${data[i].year_established}</td>
         <td>${data[i].country}</td>
-        <td>${data[i].url}</td>
+        <td><a href=${data[i].url}>${data[i].url}</a></td>
         </tr>`
         );
+
     });
   });
 }
 
 displayExchange();
-
-/*let BASE_URL = "https://api.coingecko.com/api/v3";
-let EXCHNAGE_ENDPOINT = "/exchanges";
-let urlEx = BASE_URL + EXCHNAGE_ENDPOINT;
-
-let data = "";
-
-function displayExchange() {
-
-  fetch(urlEx)
-    .then(function (res) {
-      res.json().then(function (data){
-
-          $("#myExDisplay").append(
-            `<tr>
-            <th scope="row">${i}</th>
-            <td><img width=18px src=${data[i].image}>&nbsp;<strong>${data[i].name}</stong></td>
-            <td>$${data[i].trust_score}</td>
-            <td>$${data[i].trade_volume_24h_btc}</td>
-            <td>$${data[i].year_established}</td>
-            <td>${data[i].country}</td>
-            <td>${data[i].url}</td>
-            </tr>`
-            );
-          });
-        });
-      }
-
-  displayExchange();
-
-/*  $("exchange").click(function(){
-    $("table").hide();
-  });
-
-  $("exchange").click(function(){
-    $("table2").show();
-  });
-*/
