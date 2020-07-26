36c80c20e744bc330c691ffc3e6061c9e0ec963b
Nev <67838965+NevMoney@users.noreply.github.com>
2020-07-26 14:00:51 -0400

    news page created
    page to automatically deliver news using API... api not running as of now.

---
+++ newsAPI.js
@@ -0,0 +1,37 @@
+var settings = {
+  "url": "https://api.coinstats.app/public/v1/news?skip=0&limit=10",
+  "method": "GET",
+  "timeout": 0,
+};
+
+function displayNews() {
+  $.ajax(settings).done(function (response) {
+    const myfeed = response.news;
+
+         $("#price").html(myfeed);
+
+       });
+}
+
+
+/*
+    for(let i = 0; i < response.length; i++)
+
+      $("#myNewsDisplay").append(
+        `<tr>
+
+        <td><img width=18px src=${news[i].image}>&nbsp;<strong>${news[i].title}</stong></td>
+        <td>${news[i].description}</td>
+        <td>${news[i].source} BTC</td>
+        <td>${news[i].feedDate}</td>
+        <td>${news[i].relatedCoins}</td>
+
+        </tr>`
+        );
+      });
+}
+
+
+displayNews();
+
+*/
