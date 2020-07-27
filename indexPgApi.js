a0f19f7a93fc2e4787a793e6bf88947af7665615
Nev <67838965+NevMoney@users.noreply.github.com>
2020-07-27 17:55:46 -0400

    testing

--- indexPgApi.js
+++ indexPgApi.js
@@ -1,36 +1,37 @@
 let BASE_URL = "https://api.coingecko.com/api/v3";
-let COIN_LIST_ENDPOINT = "/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=24h";
+let COIN_LIST_ENDPOINT =
+  "/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=24h";
 let url = BASE_URL + COIN_LIST_ENDPOINT;
 
 function displayAPI() {
-
-  fetch(url)
-    .then(function (res) {
-      res.json().then(function (data) {
-
-    for(let i = 0; i < data.length; i++){
-      let color24 = "";
-        if (data[i].price_change_percentage_24h > 0){
+  fetch(url).then(function (res) {
+    res.json().then(function (data) {
+      for (let i = 0; i < data.length; i++) {
+        let color24 = "";
+        if (data[i].price_change_percentage_24h > 0) {
           color24 = "positive";
-        }
-        else if (data[i].price_change_percentage_24h < 0) {
+        } else if (data[i].price_change_percentage_24h < 0) {
           color24 = "negative";
         }
 
-      mc = new Intl.NumberFormat().format(data[i].market_cap);
-      p = new Intl.NumberFormat().format(data[i].current_price);
-      v = new Intl.NumberFormat().format(data[i].total_volume);
-      cs = new Intl.NumberFormat().format(data[i].circulating_supply);
+        mc = new Intl.NumberFormat().format(data[i].market_cap);
+        p = new Intl.NumberFormat().format(data[i].current_price);
+        v = new Intl.NumberFormat().format(data[i].total_volume);
+        cs = new Intl.NumberFormat().format(data[i].circulating_supply);
 
-      $("#myDisplay").append(
-        `<tr>
-          <th scope="row" class="notbold">${i+1}</th>
-          <td><img width=16px src=${data[i].image}>&nbsp;<strong>${data[i].name}</stong></td>
+        $("#myDisplay").append(
+          `<tr>
+          <th scope="row" class="notbold">${i + 1}</th>
+          <td><img width=16px src=${data[i].image}>&nbsp;<strong>${
+            data[i].name
+          }</stong></td>
           <td>$${mc}</td>
           <td>$${p}</td>
           <td>$${v}</td>
           <td>${cs} ${data[i].symbol.toUpperCase()}</td>
-          <td class="${color24}">${data[i].price_change_percentage_24h.toFixed(2)}%</td>
+          <td class="${color24}">${data[i].price_change_percentage_24h.toFixed(
+            2
+          )}%</td>
         </tr>`
         );
       }
@@ -39,3 +40,9 @@ function displayAPI() {
 }
 
 displayAPI();
+
+$("#exchange-tab").click(function () {
+  $("#Cryptocurrencies").hide();
+  $("#Exchanges").show();
+  console.log("tab clicked");
+});
