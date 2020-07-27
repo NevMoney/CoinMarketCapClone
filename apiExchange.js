7fd820d4630de2a22f914099db2f90d5798d921f
Nev <67838965+NevMoney@users.noreply.github.com>
2020-07-27 18:42:06 -0400

    API Fixed
    tabs work right now

--- apiExchange.js
+++ apiExchange.js
@@ -2,18 +2,17 @@ let BASE_URL = "https://api.coingecko.com/api/v3";
 let EXCHNAGE_ENDPOINT = "/exchanges";
 let urlEx = BASE_URL + EXCHNAGE_ENDPOINT;
 
-
 function displayExchange() {
-
-  fetch(urlEx)
-    .then(function (res) {
-      res.json().then(function (data) {
-      for(let i = 0; i < data.length; i++)
-
-      $("#myExDisplay").append(
-        `<tr>
-        <th scope="row" class="notbold">${i+1}</th>
-        <td><img width=18px src=${data[i].image}>&nbsp;<strong>${data[i].name}</stong></td>
+  fetch(urlEx).then(function (res) {
+    res.json().then(function (data) {
+      $("#loading").hide();
+      for (let i = 0; i < data.length; i++)
+        $("#myExDisplay").append(
+          `<tr>
+        <th scope="row" class="notbold">${i + 1}</th>
+        <td><img width=18px src=${data[i].image}>&nbsp;<strong>${
+            data[i].name
+          }</stong></td>
         <td>${data[i].trust_score}</td>
         <td>${data[i].trade_volume_24h_btc.toFixed(2)} BTC</td>
         <td>${data[i].year_established}</td>
@@ -21,16 +20,14 @@ function displayExchange() {
         <td><a href=${data[i].url} target="_blank">${data[i].url}</a></td>
         </tr>`
         );
-
     });
   });
 }
 
 displayExchange();
 
-
-$("#crypto-tab").click(function() {
+$("#crypto-tab").click(function () {
   $("#Cryptocurrencies").show();
   $("#Exchanges").hide();
-  console.log("tab clicked")
+  console.log("tab clicked");
 });
