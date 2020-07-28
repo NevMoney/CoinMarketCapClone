8d817cbe1eef4491e9ba1506726c6dbceff0a094
Nev <67838965+NevMoney@users.noreply.github.com>
2020-07-27 20:28:31 -0400

    progress on news api
    fixed the delivery of API for news and started pagination on index to deliver additional assets

--- indexPgApi.js
+++ indexPgApi.js
@@ -1,4 +1,5 @@
 let BASE_URL = "https://api.coingecko.com/api/v3";
+var pgNumb = ``;
 let COIN_LIST_ENDPOINT =
   "/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=24h";
 let url = BASE_URL + COIN_LIST_ENDPOINT;
